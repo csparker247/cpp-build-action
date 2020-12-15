29189862ad3b923eb61bf8d976a320db810938a1
Seth Parker <csparker247@gmail.com>
2020-12-15 14:04:35 -0500

    Remove OpenCV stuff

--- Brewfile
+++ Brewfile
@@ -1,2 +1,1 @@
-brew 'opencv@4'
 brew 'itk'
--- main.cpp
+++ main.cpp
@@ -1,18 +1,10 @@
-#include <iostream>
-#include <opencv2/core.hpp>
 #include <itkMesh.h>
 #include <itkVector.h>
 
 int main() {
-  cv::Vec3d a{1,1,1}, b{0,0,0};
-  cv::Mat m;
-
   using Point = itk::Vector<double, 3>;
   using Traits = itk::DefaultStaticMeshTraits<Point, 3, 3, double, double, Point>;
   using Mesh = itk::Mesh<Point, 3, Traits>;
 
   auto mesh = Mesh::New();
-
-  std::cout << a[0] << b[0] <<  cv::norm(a) << std::endl;
-  std::cout << m.empty() << std::endl;
 }
