d00713419829e9ee89b08f4d6d868870c9360dcb
Seth Parker <csparker247@gmail.com>
2020-12-15 13:25:43 -0500

    Try more commands

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <opencv2/core.hpp>
-#include <iostream>
 
 int main() {
-  cv::Vec3d a, b;
-  if(a != b) {
-    return EXIT_SUCCESS;
-  }
+  cv::Vec3d a{1,1,1}, b{0,0,0};
+  cv::Mat m;
+
+  std::cout << a[0] << b[0] <<  cv::norm(a) << std::endl;
+  std::cout << m.empty() << std::endl;
 }
